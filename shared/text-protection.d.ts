b906465a784ea61123e3b91eb16315b366096745
Giles Kisife <giles_kisife@yahoo.ca>
2025-09-24 10:25:31 -0400

    feat(frontend): add local.env configuration file

--- shared/text-protection.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const TEMPLATE_START = "___TEMPLATE_START___";
-export declare const TEMPLATE_END = "___TEMPLATE_END___";
-export declare const VERBATIM_START = "___VERBATIM_START___";
-export declare const VERBATIM_END = "___VERBATIM_END___";
-export declare function separateProtectedRegions(text: string): {
-    text: string;
-    isProtected: boolean;
-}[];
-//# sourceMappingURL=text-protection.d.ts.map
